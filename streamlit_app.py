f5a4bbcd835b7c7489627656dbf239f8ab9fefe3
anng313 <dora0313@naver.com>
2025-10-02 06:49:49 +0000

    soojung

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,3 @@ st.set_page_config(page_title='함수 학습 앱', page_icon='📈', layout='cen
 st.markdown('<h1 style="text-align:center; color:#2d6cdf;">함수에 대해 알아보아요!</h1>', unsafe_allow_html=True)
 st.markdown('<p style="text-align:center; font-size:1.2em;">오른쪽 목록을 클릭하여 함수를 그려보거나 퀴즈를 맞춰보세요.</p>', unsafe_allow_html=True)
 
-st.image('https://cdn.pixabay.com/photo/2016/03/31/19/56/curve-1295705_1280.png', caption='함수 그래프 예시', use_container_width=True)
